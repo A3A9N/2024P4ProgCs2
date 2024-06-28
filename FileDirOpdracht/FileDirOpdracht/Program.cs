235c5c44fbdeb220be50fdd5007177ad0979a60c
A3A9N <144899095+A3A9N@users.noreply.github.com>
2024-06-28 15:02:26 +0200

    opdracht 9

---
+++ FileDirOpdracht/FileDirOpdracht/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DirectoryInfo dir = new DirectoryInfo(@"C:\Users\am591\Documents\GitHub\2024P4ProgCs2");
+
+        FileInfo[] files = dir.GetFiles();
+
+        Console.WriteLine("Bestanden:");
+        foreach (var file in files)
+        {
+            Console.WriteLine(file.Name);
+        }
+
+        DirectoryInfo[] directories = dir.GetDirectories();
+
+
+        Console.WriteLine("\nMappen:");
+        foreach (var directory in directories)
+        {
+            Console.WriteLine(directory.Name);
+        }
+    }
+}
