52f2744cb67b3829c8d9266713542080085473c6
A3A9N <144899095+A3A9N@users.noreply.github.com>
2024-06-28 15:22:40 +0200

    11 al af

---
+++ SplitOpdracht/SplitOpdracht/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string content = File.ReadAllText("stringsplit.txt");
+       
+     
+        string[] keyValue = content.Split(':');
+
+        
+        foreach (var item in keyValue)
+        {
+            Console.WriteLine(item);
+        }
+
+
+        string[] vakken = keyValue[1].Split(',');
+
+        Console.WriteLine("\nGesplitste vakken:");
+        foreach (var vak in vakken)
+        {
+            Console.WriteLine(vak.Trim()); 
+        }
+    }
+}
